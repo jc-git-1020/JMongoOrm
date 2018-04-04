fe293250ead5f212b397ba31e0227cd6620842dd
chenjunbin <178933579@qq.com>
2018-04-04 18:01:07 +0800

    FilterBuilder

--- mongodbormtest/src/main/java/db/common/FilterBuilder.java
+++ mongodbormtest/src/main/java/db/common/FilterBuilder.java
@@ -1,5 +1,6 @@
 package db.common;
 
+import com.mongodb.client.model.Filters;
 import org.bson.BsonDocument;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.conversions.Bson;
@@ -8,94 +9,98 @@ import java.util.LinkedList;
 
 public class FilterBuilder extends LinkedList<Bson> implements Bson {
 
+    BsonDocument doc = new BsonDocument();
+
     public <TItem> FilterBuilder eq(final String fieldName, final TItem value){
-        this.add(eq(fieldName,value));
+        this.add(Filters.eq(fieldName,value));
         return this;
     }
 
     public <TItem> FilterBuilder ne(final String fieldName, final TItem value){
-        this.add(ne(fieldName,value));
+        this.add(Filters.ne(fieldName,value));
         return this;
     }
 
     public <TItem> FilterBuilder gt(final String fieldName, final TItem value){
-        this.add(gt(fieldName,value));
+        this.add(Filters.gt(fieldName,value));
         return this;
     }
 
     public <TItem> FilterBuilder lt(final String fieldName, final TItem value){
-        this.add(lt(fieldName,value));
+        this.add(Filters.lt(fieldName,value));
         return this;
     }
 
     public <TItem> FilterBuilder gte(final String fieldName, final TItem value){
-        this.add(gte(fieldName,value));
+        this.add(Filters.gte(fieldName,value));
         return this;
     }
 
     public <TItem> FilterBuilder lte(final String fieldName, final TItem value){
-        this.add(lte(fieldName,value));
+        this.add(Filters.lte(fieldName,value));
         return this;
     }
 
     public <TItem> FilterBuilder in(final String fieldName, final TItem... values){
-        this.add(in(fieldName, values));
+        this.add(Filters.in(fieldName, values));
         return this;
     }
 
     public <TItem> FilterBuilder in(final String fieldName, final Iterable<TItem> values){
-        this.add(in(fieldName, values));
+        this.add(Filters.in(fieldName, values));
         return this;
     }
 
     public <TItem> FilterBuilder nin(final String fieldName, final TItem... values){
-        this.add(nin(fieldName, values));
+        this.add(Filters.nin(fieldName, values));
         return this;
     }
 
     public <TItem> FilterBuilder nin(final String fieldName, final Iterable<TItem> values){
-        this.add(nin(fieldName, values));
+        this.add(Filters.nin(fieldName, values));
         return this;
     }
 
     public <TItem> FilterBuilder and(final Iterable<Bson> filters){
-        this.add(and(filters));
+        this.add(Filters.and(filters));
         return this;
     }
 
     public <TItem> FilterBuilder and(final Bson... filters){
-        this.add(and(filters));
+        this.add(Filters.and(filters));
         return this;
     }
 
     public <TItem> FilterBuilder or(final Iterable<Bson> filters){
-        this.add(or(filters));
+        this.add(Filters.or(filters));
         return this;
     }
 
     public <TItem> FilterBuilder or(final Bson... filters){
-        this.add(or(filters));
+        this.add(Filters.or(filters));
         return this;
     }
 
     public <TItem> FilterBuilder not(final Bson filter){
-        this.add(not(filter));
+        this.add(Filters.not(filter));
         return this;
     }
 
-    public <TItem> FilterBuilder not(final Bson... filters){
-        this.add(not(filters));
+    public <TItem> FilterBuilder nor(final Bson... filters){
+        this.add(Filters.nor(filters));
         return this;
     }
 
-    public <TItem> FilterBuilder not(final Iterable<Bson> filters){
-        this.add(not(filters));
+    public <TItem> FilterBuilder nor(final Iterable<Bson> filters){
+        this.add(Filters.nor(filters));
         return this;
     }
 
 
     @Override
     public <TDocument> BsonDocument toBsonDocument(Class<TDocument> tDocumentClass, CodecRegistry codecRegistry) {
+
+       //BsonDocument tet = new BsonDocument(this);
 
 
 
